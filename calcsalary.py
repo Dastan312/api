b52cd97381b6298f58c21f6ec8e3ad0a3cbe3009
Dastan312 <dastanmirzayev88@gmail.com>
2023-06-06 14:01:53 +0200

    1400

--- calcsalary.py
+++ calcsalary.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Union
-from joblib import load
+
 
 
 
--- main.py
+++ main.py
@@ -1,37 +1,20 @@
 from fastapi import FastAPI
-import uvicorn
 from pydantic import BaseModel
 from joblib import load
-import pickle
+import uvicorn
+import xgboost as xgb
 from typing import List
 
 
-# ['Bedrooms', 'Living_area', 'Postcode', 'Number_of_floors','Primary_energy_consumption', 'Construction_year']
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'message': 'Hello, World'}
-
 class DataPoint(BaseModel):
-
-    Bedrooms: int 
+    Bedrooms: float 
     Living_area: float
-    Postcode: int
-    Number_of_floors: int
-    Primary_eergy_consumption: float
-    Construction_year: int
-
-
-@app.post("/predict")
-def predict(data: List[float]):
-    prediction = predict(data)
-    return {"prediction": prediction}
-
-
-
-pickle_in = open("regXGBoost.pkl","rb")
-reg=pickle.load(pickle_in)
+    Postcode: float
+    Number_of_floors: float
+    Primary_energy_consumption: float
+    Construction_year: float
 
 
 @app.get('/')
@@ -39,39 +22,17 @@ def index():
     return {'message': 'Hello, World'}
 
 
-@app.post('/predict')
-def Price(data:DataPoint):
-    data = data.dict()
-    Bedrooms=data['Bedrooms']
-    Living_area=data['Living_area']
-    Number_of_floors=data['Number_of_floors']
-    Construction_year=data['Construction_year']
+@app.post('/api/predict')
+def predict(data: List[float]):
+    data = [data]  
+    dmatrix = xgb.DMatrix(data)
+    prediction = reg.predict(dmatrix)
+    return {"prediction": prediction[0]}
 
 
-
-
-    # prediction = reg.predict([['Bedrooms', 'Living_area', 'Postcode', 'Number_of_floors','Primary_energy_consumption', 'Construction_year']])
-    # if(prediction[0]>0.5):
-    #     prediction="BAD"
-    # else:
-    #     prediction="GOOD"
-    # return {
-    #     'prediction': prediction
-    # }
-
-# 5. Run the API with uvicorn
-#    Will run on http://127.0.0.1:8000
-
-
-
-
-#http://127.0.0.1:8000/docs
+reg = xgb.XGBRegressor()  
+reg.load_model("regXGBoost.pkl") 
 
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
-
-
-#uvicorn main:app --reload
-
-   
