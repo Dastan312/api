509554615a94de16777abc4fa438fc8fcac7914f
Dastan312 <dastanmirzayev88@gmail.com>
2023-06-06 16:57:22 +0200

    done

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from fastapi import FastAPI
+import numpy as np
+import json
 from pydantic import BaseModel
 from joblib import load
 import uvicorn
 import xgboost as xgb
 from typing import List
+import pandas as pd
 
 
 app = FastAPI()
 
-class DataPoint(BaseModel):
-    Bedrooms: float 
-    Living_area: float
-    Postcode: float
-    Number_of_floors: float
-    Primary_energy_consumption: float
-    Construction_year: float
+reg = xgb.Booster()
+reg.load_model("xgb_properties") 
+
+
+# class DataPoint(BaseModel):
+#     bedrooms: float 
+#     living_area: float
+#     post_code: float
+#     number_of_floors: float
+#     primary_energy_consumption: float
+#     construction_year: float
 
 
 @app.get('/')
@@ -22,16 +29,22 @@ def index():
     return {'message': 'Hello, World'}
 
 
-@app.post('/api/predict')
-def predict(data: List[float]):
-    data = [data]  
+@app.post('/predict')
+def predict(
+    # data: DataPoint
+    bedrooms: float = 1, 
+    living_area: float = 200, 
+    post_code: float = 1000, 
+    number_of_floors: float = 2, 
+    primary_energy_consumption: float = 50, 
+    construction_year: float = 2020
+):
+    data = np.array([[bedrooms, living_area, post_code, number_of_floors, primary_energy_consumption, construction_year]])
     dmatrix = xgb.DMatrix(data)
     prediction = reg.predict(dmatrix)
-    return {"prediction": prediction[0]}
-
-
-reg = xgb.XGBRegressor()  
-reg.load_model("regXGBoost.pkl") 
+    converted_prediction = float(prediction[0])
+    print('prediction: ', converted_prediction)
+    return {"prediction": converted_prediction}
 
 
 if __name__ == '__main__':
